f79b744cc72ed7467dbaba0a1462ae07a35e499d
Frank Lanitz <frank@frank.uvena.de>
2015-02-20 19:14:35 +0100

    Add check whether sqlanydb modul can be loaded

--- geanypysql.py
+++ geanypysql.py
@@ -3,7 +3,16 @@
 
 import gtk
 import geany
-import sqlanydb
+
+
+try:
+    import sqlanydb
+    sqla = True
+except:
+    sqla = False
+
+
+
 
 class GeanyPySQLDB():
 
@@ -36,7 +45,7 @@ class GPS_sqlanywhere(GeanyPySQLDB):
             self.connection = sqlanydb.connect( userid=self.userid,
                                     password=self.password,
                                     server=self.server)
-    
+
     def disconnect(self):
         if self.connection is not None:
             self.connection.close()
@@ -48,15 +57,15 @@ class GeanyPySQL(geany.Plugin):
     __plugin_version__ = "0.1"
     __plugin_description__ = "Making an SQL client out of Geany"
     __plugin_author__ = "Frank Lanitz <frank@frank.uvena.de>"
-    
+
     db = None
     cursor = None
 
     def __init__(self):
-        
-        # Logging 
+
+        # Logging
         geany.Plugin.__init__(self)
-        
+
         # Adding menu entries
         # Add main menu entry for pluin and show it
         self.root_menu = gtk.MenuItem("_GeanyPySQL")
@@ -72,11 +81,11 @@ class GeanyPySQL(geany.Plugin):
         # Now adding items to submenu
         mi_connect = gtk.MenuItem("_Connect")
         plugin_menu.append(mi_connect)
-        
+
         mi_disconnect = gtk.MenuItem("_Disconnect")
         plugin_menu.append(mi_disconnect)
-        
-        # And callbacks 
+
+        # And callbacks
         mi_connect.connect("activate", self.on_click_connect, None)
         mi_disconnect.connect("activate", self.on_click_disconnect, None)
 
@@ -88,7 +97,7 @@ class GeanyPySQL(geany.Plugin):
     def on_click_connect(self, widget, data):
         self.db = GPS_sqlanywhere(uid='dba', pwd='sql', server='foo')
         self.db.connect()
-        
+
     def on_click_disconnect(self, widget, data):
         self.db.disconnect()
 
